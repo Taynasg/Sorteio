c073dfc4bd44706163901ca44390e8b8a990e018
Taynasg <115035153+Taynasg@users.noreply.github.com>
2023-01-30 16:42:35 -0300

    Atualizando a master

--- Sorteio.java
+++ Sorteio.java
@@ -8,11 +8,39 @@ public class Sorteio {
         System.out.println("         SORTEIO");
         System.out.println("===============================");
 
+ Random numeros = new Random();
+        int valor = numeros.nextInt(0, 101);
+        Scanner scanner = new Scanner(System.in);
 
-        Random numeros = new Random();
-        int valor = numeros.nextInt(0,101);
-        System.out.println(valor);
+        System.out.println("===============================");
+        System.out.println("         SORTEIO");
+        System.out.println("===============================");
+
+        int numeroEscolhido;
+        int contador = 1;
+        int tentativas;
+
+        do {
+            tentativas = contador += 1;
+            System.out.println();
+            System.out.println("Qual numero vai sair?");
+            numeroEscolhido = scanner.nextInt();
+
+            if (numeroEscolhido < valor) {
+                System.out.println("Chute mais auto!");
+
+            }
+            if (numeroEscolhido > valor) {
+                System.out.println("Chute mais baixo!");
+
+            }
+            if (numeroEscolhido == valor) {
+                System.out.println("Você acertou!");
+            }
+
+        } while ((valor > numeroEscolhido) || (valor < numeroEscolhido));
+
+        System.out.println("Você teve " + tentativas + " tentativas até acertar! ");
+
     }
-
-
 }
